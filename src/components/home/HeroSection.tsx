01064c366319b080cc822c60750b8c7e8b74f2df
dranie-18 <dranie18.priv@gmail.com>
2025-07-07 20:53:54 +0700

    Updated HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -54,7 +54,6 @@ const HeroSection: React.FC = () => {
         totalPropertiesForRent: propertiesforrentCount || 0,
         totalUsers: usersCount || 0
       });
-      console.error('chek:', usersCount);
     } catch (error) {
       console.error('Error fetching stats:', error);
       // Use fallback values if there's an error
